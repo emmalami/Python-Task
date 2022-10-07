7cc6ab0d6bc7365679b8cc74ee353e2eba5eeb91
Olamide John <olamide.john@outlook.com>
2022-10-07 22:31:05 +0100

    task complete

---
+++ Task2.py
@@ -0,0 +1,16 @@
+# Declare a variable with variable_name as my_var and value as 12.6
+my_var = 12.6
+print(my_var)
+print(type(my_var))
+
+# Declare an integer variable with the value of 200
+number = 200
+print(number)
+print(type(number))
+
+
+# Declare a String variable with the value of your name
+
+my_name = ("Emmanuel John")
+print(my_name)
+print(type(my_name))
