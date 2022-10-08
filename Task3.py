4384701f0c080ed7f86de3a3f5d4e9f1c88b150b
Olamide John <olamide.john@outlook.com>
2022-10-08 11:17:50 +0100

    task3

--- Task3.py
+++ Task3.py
@@ -1,14 +1,18 @@
 # Declare a raw string
-
+wishes = r"I love to get the following:\na plate of Salad\nfried dodo\nand chicken\r"
+print(wishes)
 
 
 # Declare two strings with any value
-
+os = "Android"
+typeof = "\tPhones"
 
 
 # Concatenate the two string
+devices = os + typeof
 
 
 
 
 # Print out the value of the concatenated strings
+print(devices)
