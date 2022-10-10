5d0386542bc2f0531e7c1ca154e3104891b7f1fa
Olamide John <olamide.john@outlook.com>
2022-10-10 12:31:22 +0100

    task4

---
+++ Task4.py
@@ -0,0 +1,12 @@
+# Declare two numbers with any values
+
+66
+55
+
+# Add or Subtract or Multiply the two numbers together
+
+mathcalc = 66 * 55
+
+
+# Print out the value of the operation
+print(mathcalc)
