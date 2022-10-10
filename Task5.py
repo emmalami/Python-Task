6b77d97041dc229a0599df2f7c25ce1497b96f26
Olamide John <olamide.john@outlook.com>
2022-10-10 14:03:07 +0100

    task5

---
+++ Task5.py
@@ -0,0 +1,21 @@
+# Declare a variable with value 2
+
+# Check if the variable is equal to 2, if true print out "Number is Two"
+
+# Add an else block which prints out "Number is not Two"
+
+# If the value of the declared variable is changed from two,
+# it should print "Number is not Two" in the console,
+# if it changed back to 2 it should print "Number is Two" in the console.
+
+
+number = 2
+
+if 2 == number:
+    print("Number is Two")
+# elif 4 >= number:
+   #  print("correct level")
+# elif 7 <= number:
+  #   print("correct level2")
+else:
+    print("Number is not Two")
