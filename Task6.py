f978d48ef377d2b37d66033317eac7933fa01efb
Olamide John <olamide.john@outlook.com>
2022-10-10 14:52:16 +0100

    task6

---
+++ Task6.py
@@ -0,0 +1,28 @@
+# Declare two numbers with any values
+
+#Check if the first number is greater than 50 and
+#check if the second number is less than 100, if True print out "Evaluation is True"
+#Add an else block that prints "Evaluation is False"
+
+number1 = 400
+number2 = 500
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+
+
+#Declare a string with value "python"
+#Check if the first string is equal to "testify" or "python",
+#if True prints out "Yay!! The string is a Testify Python".
+
+
+
+#Use the logical and to check if the first number is greater than 50 and the second number is less than 100.
+
+programlang = "python"
+if programlang == "python" or  "Testify":
+    print("Yay!! The string is a Testify Python")
+else:
+    print("Nah!! The string is not Testify Python")
