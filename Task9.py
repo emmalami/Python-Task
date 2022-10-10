0b93469d2eec8f236cd671850e80c2d83ea4ae65
Olamide John <olamide.john@outlook.com>
2022-10-10 18:59:41 +0100

    task9

---
+++ Task9.py
@@ -0,0 +1,18 @@
+# Create a function that prints out "Hello World"
+
+# Create a function that does nothing, an empty function
+
+# Invoke/call both functions
+def greet():
+    print("Hello World")
+greet()
+
+def emty():
+
+ emty()
+
+
+def add(num1, num2):
+    result = num1 + num2
+    print(result)
+add(2,4)
