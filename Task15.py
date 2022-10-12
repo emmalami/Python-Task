2d8a3509c9af1d77eb9d0cf955510953f2ab75ae
Olamide John <olamide.john@outlook.com>
2022-10-12 17:32:28 +0100

    task15

---
+++ Task15.py
@@ -0,0 +1,12 @@
+# Declare a list with any value and number of item/element
+
+# Print the list in console
+
+# Sort the list
+
+# Print the list in console again
+
+fruits = ["banana","cherries","apple","orange"]
+print("list:", fruits)
+fruits.sort()
+print("sort:",fruits)
