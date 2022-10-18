5947de1684b02d89dea796c652c460c24103b3d8
Olamide John <olamide.john@outlook.com>
2022-10-18 22:44:10 +0100

    Task 17

---
+++ Task17.py
@@ -0,0 +1,11 @@
+# Create a class called Human
+
+# Initialize the class
+
+class Human:
+    race = "black"
+    continent = "Afica"
+
+    def get_race_continent(self):
+        return self.race + ":" + self.continent
+
