12bb8bc0cc305c80d3f4357f20d64ab434aa0e58
Olamide John <olamide.john@outlook.com>
2022-10-18 23:05:38 +0100

    Task 18

---
+++ Task18.py
@@ -0,0 +1,17 @@
+# Create a class called Human
+
+# Add an attribute leg_count with the value of 4
+
+# Add another attribute can_walk with value of True
+
+class Human:
+    race = "black"
+    continent = "Afica"
+    leg_count = 4
+    can_walk = True
+
+    def __init__(self, name):
+        self.name = name
+
+
+
