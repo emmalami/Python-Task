c772fd91717bb74de10e9e6e6431b93576740d3c
Olamide John <olamide.john@outlook.com>
2022-10-20 22:01:01 +0100

    Task 24

--- Task24.py
+++ Task24.py
@@ -7,3 +7,28 @@
 # then invoke the drive_direction() in each of the object instances.
 # Hint: the drive_direction() method in your Car should print "Drive forward",
 # while drive_direction() in your Plane class should print "Drive Upward"
+
+import abc
+
+
+class vehicle(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def drive_direction(self):
+        pass
+
+
+class car(vehicle):
+    def drive_direction(self):
+        return "Drive forward"
+
+
+class plane(vehicle):
+    def drive_direction(self):
+        return "Drive upward"
+
+car = car()
+print(car.drive_direction())
+
+plane = plane()
+print(plane.drive_direction())
